242b0ae08f8707ae1c171d804836123f94aa416a
Reyortsed <2719928145@qq.com>
2026-03-17 15:27:17 +0800

    fix: update crosConfig

--- src/main/java/com/example/gem_fan_club_web/config/CorsConfig.java
+++ src/main/java/com/example/gem_fan_club_web/config/CorsConfig.java
@@ -7,16 +7,28 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
+
     @Profile("dev")
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer devCorsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://localhost",
+                                "capacitor://localhost",
+                                "http://localhost"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
+
     @Profile("prod")
     @Bean
     public WebMvcConfigurer prodCorsConfigurer() {
@@ -25,11 +37,15 @@ public class CorsConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://140.143.246.54:5173",
                                 "http://gemfanclub.com",
                                 "http://www.gemfanclub.com",
                                 "https://gemfanclub.com",
-                                "https://www.gemfanclub.com"
+                                "https://www.gemfanclub.com",
+                                "http://140.143.246.54",
+                                "http://140.143.246.54:5173",
+                                "https://localhost",
+                                "capacitor://localhost",
+                                "http://localhost"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
